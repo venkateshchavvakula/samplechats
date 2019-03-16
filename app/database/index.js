7bcf0a241c07e23413b24fbd67fa2a8f96e41ab6
venkateshchavvakula198 <venkateshchavvakula198@gmail.com>
2019-03-16 13:29:05 +0530

    modifications

--- app/database/index.js
+++ app/database/index.js
@@ -6,12 +6,7 @@ var logger 		= require('../logger');
 
 // Connect to the database
 // construct the database URI and encode username and password.
-var dbURI = "mongodb://" + 
-			encodeURIComponent(config.db.username) + ":" + 
-			encodeURIComponent(config.db.password) + "@" + 
-			config.db.host + ":" + 
-			config.db.port + "/" + 
-			config.db.name;
+var dbURI ="mongodb://venki519:venki519@ds161285.mlab.com:61285/chatserver";
 Mongoose.connect(dbURI, { useNewUrlParser: true });
 
 // Throw an error if the connection fails
